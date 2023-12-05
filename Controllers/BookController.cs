d378dfba375d241812d49376cfdd68c39eb7b291
Fairuz Sulaiman <fairuz.sulaiman@utm.my>
2023-12-05 09:07:05 +0800

    add function create update delete view-all view-detail

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPI_LZS.Models;
 
 namespace WebAPI_LZS.Controllers
@@ -14,5 +15,100 @@ namespace WebAPI_LZS.Controllers
         {
             _dbContext = dbContext;
         }
+
+        //untuk senaraikan semua data buku
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetAllBook()
+        {
+            if(_dbContext.Books == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Books.ToListAsync();
+        }
+
+        //untuk dapat satu jenis rekod buku sahaja
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookDetail(int id)
+        {
+            if(_dbContext.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _dbContext.Books.FindAsync(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // tambah satu rekod baru buku
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBookRecord(Book book)
+        {
+            _dbContext.Books.Add(book);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBookDetail), new { id = book.Id}, book);
+        }
+
+        // Put Method 
+        [HttpPut]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if(id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(book).State = EntityState.Modified; 
+            
+            try {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BooksExists(id))
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        private bool BooksExists(long id)
+        {
+            return (_dbContext.Books?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        // padam rekod
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            if (_dbContext.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _dbContext.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Books.Remove(book);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,9 +1,11 @@
-﻿using System.Security.Principal;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
 
 namespace WebAPI_LZS.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
         public string? BookTitle { get; set; }
         public string? Genre { get; set; }
